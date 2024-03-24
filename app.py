dd2bd69e2dbbf00a05c239efb09ad09f78ec4cd0
gckang <76260739+gckang@users.noreply.github.com>
2024-03-24 09:55:23 -0400

    can look graph only when nodes, edges, and matrix are hard coded

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, jsonify
-# from flask_visjs import VisJS4, Network
 from simulation import Simulation
 from l_before_r import LbeforeR
 from expected_movements import ExpectedMovements
@@ -8,10 +7,8 @@ import numpy as np
 
 app = Flask(__name__)
 
-# VisJS4().init_app(app)
-
-number_states = None
-probability_matrix = None
+number_states = 0
+probability_matrix = [[0.0] * number_states for x in range(number_states)]
 network = None
 invariant_sim = None
 
@@ -19,26 +16,25 @@ invariant_sim = None
 
 def invariant():
     global number_states, probability_matrix
-    number_states = 0  # Example number of states
-    probability_matrix = [[]]
-    # probability_matrix = [[0.2, 0.5, 0.3], [0.4, 0.3, 0.3], [0.1, 0.2, 0.7]]  # Example probability matrix
+    # number_states = 0
+    # probability_matrix = [[]]
+    probability_matrix = [[0.2, 0.5, 0.3], [0.4, 0.3, 0.3], [0.1, 0.2, 0.7]]  # Example probability matrix
     if request.method == "POST":
         number_states = int(request.form.get("numberStatesInput"))
-        probability_matrix = [[0.0] * number_states for x in range(number_states)]
-        # for x in number_states:
-        #     network.add_node(x, shape='circle')
-    return render_template("invariant.html", number_states=number_states, probability_matrix=probability_matrix, network=network)
+
+    print("Probability Matrix in Flask:", probability_matrix) 
+    return render_template("invariant.html", number_states=number_states, probability_matrix=probability_matrix)
 
 @app.route("/table", methods=["POST"])
 def process_invariant_table():
     global number_states, probability_matrix
+    probability_matrix = [[0.0] * number_states for x in range(number_states)]
     for i in range(number_states):
         for j in range(number_states):
             probability_matrix[i][j] = float(request.form.get(f"probability{i}{j}"))
-            # if probability_matrix[i][j] > 0:
-            #     network.add_edge(i, j, value=probability_matrix[i][j])
+
     print(probability_matrix)
-    return render_template("invariant.html", number_states=number_states, probability_matrix=probability_matrix, network=network)
+    return render_template("invariant.html", number_states=number_states, probability_matrix=probability_matrix)
 
 
 # @app.route("/simulate_step", methods=['POST'])
