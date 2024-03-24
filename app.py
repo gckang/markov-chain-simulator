c4f6d5c78104811aaed5c23bca244f538d96faf2
gckang <76260739+gckang@users.noreply.github.com>
2024-03-24 10:15:43 -0400

    combined irreducible and invariant

--- app.py
+++ app.py
@@ -7,34 +7,32 @@ import numpy as np
 
 app = Flask(__name__)
 
-number_states = 0
-probability_matrix = [[0.0] * number_states for x in range(number_states)]
-network = None
-invariant_sim = None
+# number_states = 0
+# probability_matrix = [[0.0] * number_states for x in range(number_states)]
+# network = None
+# invariant_sim = None
 
 @app.route("/", methods=["GET", "POST"])
 
 def invariant():
-    global number_states, probability_matrix
-    # number_states = 0
-    # probability_matrix = [[]]
-    probability_matrix = [[0.2, 0.5, 0.3], [0.4, 0.3, 0.3], [0.1, 0.2, 0.7]]  # Example probability matrix
+    number_states = 0
+    probability_matrix = [[0.2, 0.4, 0.4], [0.4, 0.5, 0.1], [0.6, 0.3, 0.1]]  # Example probability matrix
     if request.method == "POST":
         number_states = int(request.form.get("numberStatesInput"))
 
     print("Probability Matrix in Flask:", probability_matrix) 
-    return render_template("invariant.html", number_states=number_states, probability_matrix=probability_matrix)
+    return render_template("irreducible.html", number_states=number_states, probability_matrix=probability_matrix)
 
-@app.route("/table", methods=["POST"])
-def process_invariant_table():
-    global number_states, probability_matrix
-    probability_matrix = [[0.0] * number_states for x in range(number_states)]
-    for i in range(number_states):
-        for j in range(number_states):
-            probability_matrix[i][j] = float(request.form.get(f"probability{i}{j}"))
+# @app.route("/table", methods=["POST"])
+# def process_invariant_table():
+#     global number_states, probability_matrix
+#     probability_matrix = [[0.0] * number_states for x in range(number_states)]
+#     for i in range(number_states):
+#         for j in range(number_states):
+#             probability_matrix[i][j] = float(request.form.get(f"probability{i}{j}"))
 
-    print(probability_matrix)
-    return render_template("invariant.html", number_states=number_states, probability_matrix=probability_matrix)
+#     print(probability_matrix)
+#     return render_template("invariant.html", number_states=number_states, probability_matrix=probability_matrix)
 
 
 # @app.route("/simulate_step", methods=['POST'])
