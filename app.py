f3741d7568c1db9e0fdf470b22f33bba912a15c0
gckang <76260739+gckang@users.noreply.github.com>
2024-03-24 07:26:31 -0400

    attempted using flask_visjs (commented out because not working)

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+# from flask_visjs import VisJS4, Network
 from simulation import Simulation
 from l_before_r import LbeforeR
 from expected_movements import ExpectedMovements
@@ -7,9 +8,12 @@ import numpy as np
 
 app = Flask(__name__)
 
+# VisJS4().init_app(app)
+
 number_states = None
 probability_matrix = None
-# invariant_sim = None
+network = None
+invariant_sim = None
 
 @app.route("/", methods=["GET", "POST"])
 
@@ -21,28 +25,27 @@ def invariant():
     if request.method == "POST":
         number_states = int(request.form.get("numberStatesInput"))
         probability_matrix = [[0.0] * number_states for x in range(number_states)]
-    return render_template("invariant.html", number_states=number_states, probability_matrix=probability_matrix)
+        # for x in number_states:
+        #     network.add_node(x, shape='circle')
+    return render_template("invariant.html", number_states=number_states, probability_matrix=probability_matrix, network=network)
 
-@app.route("/invariant_table", methods=["POST"])
+@app.route("/table", methods=["POST"])
 def process_invariant_table():
     global number_states, probability_matrix
     for i in range(number_states):
         for j in range(number_states):
             probability_matrix[i][j] = float(request.form.get(f"probability{i}{j}"))
-
+            # if probability_matrix[i][j] > 0:
+            #     network.add_edge(i, j, value=probability_matrix[i][j])
     print(probability_matrix)
-    return render_template("invariant.html", number_states=number_states, probability_matrix=probability_matrix)
+    return render_template("invariant.html", number_states=number_states, probability_matrix=probability_matrix, network=network)
 
 
- 
 # @app.route("/simulate_step", methods=['POST'])
-# def simulate_step():
+# def simulate_step_invariant():
 #     global number_states, probability_matrix, invariant_sim
-#     data = request.get_json()
-#     number_states = int(data['number_states'])
 #     if invariant_sim is None:
 #         # Initialize invariant_sim if not initialized yet
-#         probability_matrix = [[0.0] * number_states for _ in range(number_states)]
 #         invariant_sim = InvariantSimulation(number_states, probability_matrix)
     
 #     invariant_sim.simulation_move()
@@ -54,7 +57,7 @@ def process_invariant_table():
 #     #     'edges': updated_edges
 #     # }
 #     # return jsonify(response)
-#     return jsonify({'message': 'Step simulated successfully'})
+#     return render_template("invariant.html", number_states=number_states, probability_matrix=probability_matrix, network=network)
 
 @app.route("/random-walk")
 def random_walk():
